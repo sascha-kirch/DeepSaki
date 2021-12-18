dac934a64e857a8bb8078ddf549df6837a53d18d
SaKi1309 <72045951+SaKi1309@users.noreply.github.com>
2021-12-18 10:29:23 +0100

    Create environment.py

---
+++ utils/environment.py
@@ -0,0 +1,76 @@
+# Detect hardware
+from tensorflow.python.client import device_lib
+import tensorflow as tf
+from tensorflow.keras.mixed_precision import Policy as mixed_precision
+
+def DetectHw():
+  '''
+  detects HW accelerators if present, initializes them and configures the distribution strategy
+  args:
+    None
+  return:
+    strategy: pointer to the distribution strategy configuration object
+    runtime_environment: Info string describing the HW that might be used for conditions, i.e. "TPU, "GPU", "CPU"
+    hw_accelerator_handle: pointer to the HW accelerator
+  ''' 
+  hw_accelerator_handle = None
+  try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver() # TPU detection
+    hw_accelerator_handle = tpu
+  except ValueError:
+    tpu = None
+    gpus = tf.config.experimental.list_logical_devices("GPU")
+    hw_accelerator_handle = gpus
+      
+  # Select appropriate distribution strategy
+  if tpu:
+    runtime_environment = "TPU"
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.TPUStrategy(tpu) # Going back and forth between TPU and host is expensive. Better to run 128 batches on the TPU before reporting back.
+    print('Running on TPU ', tpu.cluster_spec().as_dict()['worker'])  
+  elif len(gpus) > 1:
+    runtime_environment = "GPU"
+    strategy = tf.distribute.MirroredStrategy([gpu.name for gpu in gpus])
+    print('Running on multiple GPUs ', [gpu.name for gpu in gpus])
+  elif len(gpus) == 1:
+    runtime_environment = "GPU"
+    strategy = tf.distribute.get_strategy() # default strategy that works on CPU and single GPU
+    print('Running on single GPU ', gpus[0].name)
+  else:
+    runtime_environment = "CPU"
+    strategy = tf.distribute.get_strategy() # default strategy that works on CPU and single GPU
+    print('Running on CPU')
+  
+  print("Number of accelerators: ", strategy.num_replicas_in_sync)
+  print("____________________________________________________________________________________")
+  print("Device List: ")
+  print(device_lib.list_local_devices())
+
+  return strategy, runtime_environment, hw_accelerator_handle
+
+
+def EnableXlaAcceleration():
+  '''
+  Enables compiler acceleration for linear algebra
+  '''
+  tf.config.optimizer.set_jit(True)
+  print('Accelerated Linear Algebra enabled')
+  
+  
+def EnableMixedPrecision():
+  '''
+  Sets mixed precission depending on the presence of an HW accelerator. TPU uses bfloat format.
+  '''
+  try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver() # TPU detection
+  except ValueError:
+    tpu = None
+
+  if tpu:
+    policyConfig = 'mixed_bfloat16'
+  else: 
+    policyConfig = 'mixed_float16'
+  policy = tf.keras.mixed_precision.Policy(policyConfig)
+  tf.keras.mixed_precision.set_global_policy(policy)
+  print('Mixed precision enabled to {}'.format(policyConfig))
