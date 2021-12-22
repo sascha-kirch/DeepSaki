b56cc7217844bcede3d07864e09b58f843845a89
Sascha <72045951+SaKi1309@users.noreply.github.com>
2021-12-22 16:48:55 +0100

    Update __init__.py

--- layers/__init__.py
+++ layers/__init__.py
@@ -2,5 +2,7 @@ from DeepSaki.layers.pooling import GlobalSumPooling2D
 from DeepSaki.layers.padding import ReflectionPadding2D
 from DeepSaki.layers.layer_composites import Conv2DSplitted
 from DeepSaki.layers.layer_composites import Conv2DBlock
+from DeepSaki.layers.layer_composites import DenseBlock
+
 
 from DeepSaki.layers import helper
