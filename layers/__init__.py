37a259da3fee57ac545297a7011752a425af9751
Sascha <72045951+SaKi1309@users.noreply.github.com>
2021-12-22 01:57:30 +0100

    Update __init__.py

--- layers/__init__.py
+++ layers/__init__.py
@@ -1,8 +1,4 @@
 from DeepSaki.layers.pooling import GlobalSumPooling2D
 from DeepSaki.layers.padding import ReflectionPadding2D
 
-from DeepSaki.layers.helper import PlotLayer
-from DeepSaki.layers.helper import GetInitializer
-from DeepSaki.layers.helper import pad_func
-from DeepSaki.layers.helper import activation_func
-from DeepSaki.layers.helper import dropout_func
+from DeepSaki.layers import helper
