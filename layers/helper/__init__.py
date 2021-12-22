ff3fe6553b29149c6eb0405458ebe686ce8fc172
Sascha <72045951+SaKi1309@users.noreply.github.com>
2021-12-22 01:56:49 +0100

    Create __init__.py

---
+++ layers/helper/__init__.py
@@ -0,0 +1,5 @@
+from DeepSaki.layers.helper.layer_helper import PlotLayer
+from DeepSaki.layers.helper.layer_helper import GetInitializer
+from DeepSaki.layers.helper.layer_helper import pad_func
+from DeepSaki.layers.helper.layer_helper import activation_func
+from DeepSaki.layers.helper.layer_helper import dropout_func
