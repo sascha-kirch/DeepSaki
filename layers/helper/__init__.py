6def999070b733d48f34aa158bcb9e4ac514ae21
Sascha <72045951+SaKi1309@users.noreply.github.com>
2021-12-23 00:19:30 +0100

    Update __init__.py

--- layers/helper/__init__.py
+++ layers/helper/__init__.py
@@ -1,5 +1,4 @@
 from DeepSaki.layers.helper.layer_helper import PlotLayer
 from DeepSaki.layers.helper.layer_helper import GetInitializer
 from DeepSaki.layers.helper.layer_helper import pad_func
-from DeepSaki.layers.helper.layer_helper import activation_func
 from DeepSaki.layers.helper.layer_helper import dropout_func
