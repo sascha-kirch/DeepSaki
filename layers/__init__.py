8e31f600a2165bebfd24f7cb6df7a1ae3f03080a
Sascha <72045951+SaKi1309@users.noreply.github.com>
2021-12-22 17:45:26 +0100

    Update __init__.py

--- layers/__init__.py
+++ layers/__init__.py
@@ -3,6 +3,8 @@ from DeepSaki.layers.padding import ReflectionPadding2D
 from DeepSaki.layers.layer_composites import Conv2DSplitted
 from DeepSaki.layers.layer_composites import Conv2DBlock
 from DeepSaki.layers.layer_composites import DenseBlock
+from DeepSaki.layers.layer_composites import DownSampleBlock
+from DeepSaki.layers.layer_composites import UpSampleBlock
 
 
 from DeepSaki.layers import helper
