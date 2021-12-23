a1efc5307f1bcd63f6bc228274137c1686c51c36
Sascha <72045951+SaKi1309@users.noreply.github.com>
2021-12-23 12:18:11 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+exec(open('DeepSaki/version.py').read())
+
+# https://setuptools.pypa.io/en/latest/references/keywords.html
+
+setup(
+    name='DeepSaki',
+    version=__version__,    
+    description='Reusable machine learning code',
+    url='https://github.com/SaKi1309/DeepSaki',
+    author='Sascha Kirch',
+    author_email='susch130993@googlemail.com',
+    license='MIT',
+    packages=['DeepSaki',
+              'DeepSaki.optimizer',
+              'DeepSaki.layers',
+              'DeepSaki.layers.helper',
+              'DeepSaki.models',
+              'DeepSaki.initializer',
+              'DeepSaki.loss',
+              'DeepSaki.utils',
+              'DeepSaki.regularization',
+             ],
+    install_requires=['tensorflow >= 2.7',
+                      'numpy',
+                      'tensorflow_addons',
+                      'pandas',
+                      ],
+    extra_requires=['matplotlib'
+                    ],
+  
+    # https://pypi.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
