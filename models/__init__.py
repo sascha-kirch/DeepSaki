e8941f5c27eb7314414703607da5f111956f8e7f
Sascha <72045951+SaKi1309@users.noreply.github.com>
2021-12-23 09:43:31 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from DeepSaki.models.discriminators import LayoutContentDiscriminator
 from DeepSaki.models.discriminators import PatchDiscriminator
 
 from DeepSaki.models.autoencoders import UNet
+from DeepSaki.models.autoencoders import ResNet
