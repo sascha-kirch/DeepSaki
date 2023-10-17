9c840405dbe39e84315a5086dcd7d103bdf13381
sascha-kirch <susch130993@googlemail.com>
2023-10-17 17:03:41 +0200

    update config for documentation

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -7,28 +7,42 @@ import mkdocs_gen_files
 nav = mkdocs_gen_files.Nav()
 
 package_name = "DeepSaki"
+skip_files = ["__main__", "__version__","__init__"]
 
-for path in sorted(Path(package_name).rglob("*.py")):
+for path in sorted(Path(package_name).rglob("__init__.py")):
     module_path = path.relative_to(".").with_suffix("")
     doc_path = path.relative_to(".").with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
 
     parts = tuple(module_path.parts)
 
-    if parts[-1] == "__init__":
-        parts = parts[:-1]
-        doc_path = doc_path.with_name("index.md")
-        full_doc_path = full_doc_path.with_name("index.md")
-    elif parts[-1] in ["__main__", "version"]:
-        continue
+    parts = parts[:-1]
+    doc_path = doc_path.with_name("index.md")
+    full_doc_path = full_doc_path.with_name("index.md")
+
+
+    # remove top-level package name
+    #parts = parts[1::]
+
+    #if len(parts)<1:
+    #    continue
+
 
     nav[parts] = doc_path.as_posix()
 
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-        ident = ".".join(parts)
-        fd.write(f"::: {ident}")
+        for file in module_path.parent.glob("*.py"):
+            if file.stem in skip_files:
+                #print(f"skipped: {file}")
+                continue
+            #print(f"chosen: {file}")
+            ident = ".".join([*parts, file.stem])
+            fd.write(f"::: {ident}\n")
+            #fd.write("    options:\n")
+            #fd.write("        ignore_init_summary: true\n")
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, Path("../") / path)
+
+        mkdocs_gen_files.set_edit_path(full_doc_path, Path("../") / path)
 
 with mkdocs_gen_files.open(f"reference/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
